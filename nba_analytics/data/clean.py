980f1bb4ee7fc1852debdced7681ffd6abc103d7
sean-r-duffy <duffy.sean.14@gmail.com>
2024-07-02 00:22:07 -0400

    Combine data cleaning function in clean.py

--- nba_analytics/data/clean.py
+++ nba_analytics/data/clean.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def clean_team_stats(df=None):
@@ -19,5 +20,39 @@ def clean_team_stats(df=None):
         df = df.drop(columns=[column])
     df = df.rename(columns={'W/L%': 'W%'})
     df.to_csv('../data/interim/NBA_team_stats_1979-2024_clean.csv')
+
+    return df
+
+
+def clean_combine_data(df=None):
+    if df is None:
+        df = pd.read_csv('../../data/external/NBA_combine_stats_2000-2024.csv')
+
+    def height_to_inches(height):
+        if pd.isnull(height):
+            return np.nan
+        parts = height.split("'")
+        feet = parts[0].strip()
+        inches = parts[1].replace('"', '').strip()
+        return int(feet) * 12 + float(inches)
+
+    df.replace('-', np.nan, inplace=True)
+    df.replace('-%', np.nan, inplace=True)
+    df['height_wo_shoes'] = df['height_wo_shoes'].apply(height_to_inches)
+    df['height_w_shoes'] = df['height_w_shoes'].apply(height_to_inches)
+    df['standing_reach'] = df['standing_reach'].apply(height_to_inches)
+    df['wingspan'] = df['wingspan'].apply(height_to_inches)
+    df["bodyfat%"] = df["bodyfat%"].str.replace("%", "").astype('float')
+
+    cols = ['bodyfat%', 'hand_len_in', 'hand_width_in',
+            'height_wo_shoes', 'height_w_shoes', 'standing_reach', 'weight_lbs',
+            'wingspan', 'lane_agility_s', 'shuttle_run_s', 'three_q_sprint_s',
+            'standing_vert_in', 'max_vert_in', 'max_bench_reps']
+
+    for col in cols:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+
+    df = df.dropna(subset=cols, how='all').reset_index(drop=True)
+    df.to_csv('../../data/interim/combine_stats_trimmed.csv')
 
     return df
