e11b5b434a03256efd8a575c8b17c00adc57cda3
Pazin Tarasansombat <tarasansombat.p@northeastern.edu>
2024-08-02 01:18:43 -0400

    Fixed aggregation function in win_prediction

--- src/models/win_prediction.py
+++ src/models/win_prediction.py
@@ -47,6 +47,10 @@ def weighted_avg_2(group, weight_col, stat_cols):
     return pd.Series(d)
 
 def agg_by_pos(team_stats, player_stats):
+    team_stats = team_stats[['Season', 'Team', 'W/L%']]
+    columns_drop = ['Unnamed: 0','seas_id','g','gs','age','experience']
+    player_stats = player_stats.drop(columns=columns_drop)
+
     player_metrics = ["fg_per_100_poss", "fga_per_100_poss", "x3p_per_100_poss", "x3pa_per_100_poss", "x2p_per_100_poss", "x2pa_per_100_poss", "ft_per_100_poss", "fta_per_100_poss", "orb_per_100_poss", "drb_per_100_poss", "trb_per_100_poss", "ast_per_100_poss", "stl_per_100_poss", "blk_per_100_poss", "tov_per_100_poss", "pf_per_100_poss", "pts_per_100_poss", "o_rtg", "d_rtg", "avg_dist_fga", "percent_fga_from_x2p_range", "percent_fga_from_x0_3_range", "percent_fga_from_x3_10_range", "percent_fga_from_x10_16_range", "percent_fga_from_x16_3p_range", "percent_fga_from_x3p_range", "fg_percent_from_x2p_range", "fg_percent_from_x0_3_range", "fg_percent_from_x3_10_range", "fg_percent_from_x10_16_range", "fg_percent_from_x16_3p_range", "fg_percent_from_x3p_range", "percent_assisted_x2p_fg", "percent_assisted_x3p_fg", "percent_dunks_of_fga", "num_of_dunks", "percent_corner_3s_of_3pa", "corner_3_point_percent", "num_heaves_attempted", "num_heaves_made", "per", "x3p_ar", "f_tr", "orb_percent", "drb_percent", "trb_percent", "ast_percent", "stl_percent", "blk_percent", "tov_percent", "usg_percent", "ows", "dws", "ws", "ws_48", "obpm", "dbpm", "bpm", "vorp"]
 
     result = player_stats.groupby(['season','player_id']).apply(weighted_avg, weight_col='mp', stat_cols=player_metrics).reset_index()
