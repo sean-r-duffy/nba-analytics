7f2247ac50241d2ef16b93f36c3811abe8f51e7f
sean-r-duffy <duffy.sean.14@gmail.com>
2024-08-04 15:04:41 -0400

    Update win_prediction.pkl to ridge regression model (temporary). Update win_prediction.calculate_top_players_ui.

--- src/models/win_prediction.py
+++ src/models/win_prediction.py
@@ -1,15 +1,12 @@
 import joblib
+import json
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from src.models.nn_search import get_nba_projection
 
-def calculate_top_players_ui(selected_team: str, available_players:list) -> dict:
-    # fill with model
-    return {"Player": available_players[:4],
-            "Win%": [.89, .78, .67, .45]}
 
-def get_df(player_df, team_df):
+def get_df(player_df: pd.DataFrame, team_df: pd.DataFrame):
     # Collect player df
     if isinstance(player_df, pd.DataFrame):
         df = player_df.copy()
@@ -29,10 +26,6 @@ def get_df(player_df, team_df):
     return player_df, team_df
 
 
-def return_roster_stats(player_df, team_df, players, team, years):
-    pass
-
-
 def weighted_avg(group, weight_col, stat_cols):
     d = {}
     for col in stat_cols:
@@ -40,26 +33,41 @@ def weighted_avg(group, weight_col, stat_cols):
     d[weight_col] = group[weight_col].sum()
     return pd.Series(d)
 
+
 def weighted_avg_2(group, weight_col, stat_cols):
     d = {}
     for col in stat_cols:
         d[col] = (group[col] * group[weight_col]).sum() / group[weight_col].sum()
     return pd.Series(d)
 
+
 def agg_by_pos(team_stats, player_stats):
     team_stats = team_stats[['Season', 'Team', 'W/L%']]
     columns_drop = ['Unnamed: 0','seas_id','g','gs','age','experience']
     player_stats = player_stats.drop(columns=columns_drop)
 
-    player_metrics = ["fg_per_100_poss", "fga_per_100_poss", "x3p_per_100_poss", "x3pa_per_100_poss", "x2p_per_100_poss", "x2pa_per_100_poss", "ft_per_100_poss", "fta_per_100_poss", "orb_per_100_poss", "drb_per_100_poss", "trb_per_100_poss", "ast_per_100_poss", "stl_per_100_poss", "blk_per_100_poss", "tov_per_100_poss", "pf_per_100_poss", "pts_per_100_poss", "o_rtg", "d_rtg", "avg_dist_fga", "percent_fga_from_x2p_range", "percent_fga_from_x0_3_range", "percent_fga_from_x3_10_range", "percent_fga_from_x10_16_range", "percent_fga_from_x16_3p_range", "percent_fga_from_x3p_range", "fg_percent_from_x2p_range", "fg_percent_from_x0_3_range", "fg_percent_from_x3_10_range", "fg_percent_from_x10_16_range", "fg_percent_from_x16_3p_range", "fg_percent_from_x3p_range", "percent_assisted_x2p_fg", "percent_assisted_x3p_fg", "percent_dunks_of_fga", "num_of_dunks", "percent_corner_3s_of_3pa", "corner_3_point_percent", "num_heaves_attempted", "num_heaves_made", "per", "x3p_ar", "f_tr", "orb_percent", "drb_percent", "trb_percent", "ast_percent", "stl_percent", "blk_percent", "tov_percent", "usg_percent", "ows", "dws", "ws", "ws_48", "obpm", "dbpm", "bpm", "vorp"]
+    player_metrics = ["fg_per_100_poss", "fga_per_100_poss", "x3p_per_100_poss", "x3pa_per_100_poss",
+                      "x2p_per_100_poss", "x2pa_per_100_poss", "ft_per_100_poss", "fta_per_100_poss",
+                      "orb_per_100_poss", "drb_per_100_poss", "trb_per_100_poss", "ast_per_100_poss",
+                      "stl_per_100_poss", "blk_per_100_poss", "tov_per_100_poss", "pf_per_100_poss", "pts_per_100_poss",
+                      "o_rtg", "d_rtg", "avg_dist_fga", "percent_fga_from_x2p_range", "percent_fga_from_x0_3_range",
+                      "percent_fga_from_x3_10_range", "percent_fga_from_x10_16_range", "percent_fga_from_x16_3p_range",
+                      "percent_fga_from_x3p_range", "fg_percent_from_x2p_range", "fg_percent_from_x0_3_range",
+                      "fg_percent_from_x3_10_range", "fg_percent_from_x10_16_range", "fg_percent_from_x16_3p_range",
+                      "fg_percent_from_x3p_range", "percent_assisted_x2p_fg", "percent_assisted_x3p_fg",
+                      "percent_dunks_of_fga", "num_of_dunks", "percent_corner_3s_of_3pa", "corner_3_point_percent",
+                      "num_heaves_attempted", "num_heaves_made", "per", "x3p_ar", "f_tr", "orb_percent", "drb_percent",
+                      "trb_percent", "ast_percent", "stl_percent", "blk_percent", "tov_percent", "usg_percent", "ows",
+                      "dws", "ws", "ws_48", "obpm", "dbpm", "bpm", "vorp"]
 
-    result = player_stats.groupby(['season','player_id']).apply(weighted_avg, weight_col='mp', stat_cols=player_metrics).reset_index()
+    result = player_stats.groupby(['season', 'player_id']).apply(weighted_avg, weight_col='mp',
+                                                                 stat_cols=player_metrics).reset_index()
     result['season'] = result['season'] + 1
-    result.drop(columns=['mp'],inplace=True)
+    result.drop(columns=['mp'], inplace=True)
     result[(result['season'] == 2023) & (result['player_id'] == 5026)]
 
     player_stats_adjusted = player_stats.copy()
-    player_stats_adjusted['full_season'] = player_stats_adjusted['season'].apply(lambda x: f"{x-1}-{str(x)[-2:]}")
+    player_stats_adjusted['full_season'] = player_stats_adjusted['season'].apply(lambda x: f"{x - 1}-{str(x)[-2:]}")
     player_stats_adjusted = player_stats_adjusted[player_stats_adjusted['season'] != 1997]
 
     merged_df = pd.merge(player_stats_adjusted, result, on=['season', 'player_id'], suffixes=('', '_df2'), how='left')
@@ -81,16 +89,17 @@ def agg_by_pos(team_stats, player_stats):
     for pos in position_columns:
         # Filter the DataFrame for players playing the current position
         df_pos = df[df[pos] == 1]
-    
+
         # Group by team and season, and apply the weighted average function
-        grouped = df_pos.groupby(['full_season', 'tm']).apply(weighted_avg_2, weight_col='mp', stat_cols=stat_columns).reset_index()
-    
+        grouped = df_pos.groupby(['full_season', 'tm']).apply(weighted_avg_2, weight_col='mp',
+                                                              stat_cols=stat_columns).reset_index()
+
         # Add position prefix to the stat columns
         grouped = grouped.rename(columns={col: f'{pos}_{col}' for col in stat_columns})
-    
+
         # Append the result to the results list
         results.append(grouped)
-    
+
     # Start with the first result as the base DataFrame
     final_df = results[0]
 
@@ -106,76 +115,102 @@ def agg_by_pos(team_stats, player_stats):
 
     return final_df
 
-"""def agg_by_pos(player_df, team_df, save_file=False, use_pca=False, pca_model_path=None, output_dir=None):
-    df, team_df = get_df(player_df, team_df)
 
-    # Columns not containing player stats
-    non_stats_columns = ['g', 'gs', 'mp', 'tm', 'season', 'player_id', 'player', ]
+stat_columns = [
+    'fg_per_100_poss', 'fga_per_100_poss', 'x3p_per_100_poss', 'x3pa_per_100_poss',
+    'x2p_per_100_poss', 'x2pa_per_100_poss', 'ft_per_100_poss', 'fta_per_100_poss',
+    'orb_per_100_poss', 'drb_per_100_poss', 'trb_per_100_poss', 'ast_per_100_poss',
+    'stl_per_100_poss', 'blk_per_100_poss', 'tov_per_100_poss', 'pf_per_100_poss',
+    'pts_per_100_poss', 'o_rtg', 'd_rtg', 'avg_dist_fga',
+    'percent_fga_from_x2p_range', 'percent_fga_from_x0_3_range',
+    'percent_fga_from_x3_10_range', 'percent_fga_from_x10_16_range',
+    'percent_fga_from_x16_3p_range', 'percent_fga_from_x3p_range',
+    'fg_percent_from_x2p_range', 'fg_percent_from_x0_3_range',
+    'fg_percent_from_x3_10_range', 'fg_percent_from_x10_16_range',
+    'fg_percent_from_x16_3p_range', 'fg_percent_from_x3p_range',
+    'percent_assisted_x2p_fg', 'percent_assisted_x3p_fg',
+    'percent_dunks_of_fga', 'num_of_dunks', 'percent_corner_3s_of_3pa',
+    'corner_3_point_percent', 'num_heaves_attempted', 'num_heaves_made',
+    'per', 'x3p_ar', 'f_tr', 'orb_percent', 'drb_percent', 'trb_percent',
+    'ast_percent', 'stl_percent', 'blk_percent', 'tov_percent', 'usg_percent',
+    'ows', 'dws', 'ws', 'ws_48', 'obpm', 'dbpm', 'bpm', 'vorp'
+]
+stat_columns_PG = ['pos_PG_' + stat for stat in stat_columns]
+stat_columns_SG = ['pos_SG_' + stat for stat in stat_columns]
+stat_columns_PF = ['pos_PF_' + stat for stat in stat_columns]
+stat_columns_SF = ['pos_SF_' + stat for stat in stat_columns]
+stat_columns_C = ['pos_C_' + stat for stat in stat_columns]
 
-    # Offsetting data so that player performances are from previous season
-    df = df.drop(columns=['Unnamed: 0', 'seas_id', 'age'])
-    seasons_df = df[['season', 'tm', 'player_id']]
-    offset_df = df.drop(columns=['tm'])
-    offset_df['season'] = offset_df['season'] + 1
-    df = pd.merge(seasons_df, offset_df, on=['season', 'player_id'])
 
-    # Transforms stats columns into principal components
-    if use_pca:
-        assert isinstance(pca_model_path, str), 'Must include directory in which to save PCA model'
-        scaler = StandardScaler()
-        scaled_df = scaler.fit_transform(df.drop(columns=non_stats_columns))
-        pca = PCA(n_components=30, random_state=41)
-        pc_df = pca.fit_transform(scaled_df)
-        joblib.dump(pca, f'{pca_model_path}/player_pca.pkl')
-        df_subset = df[
-            ['g', 'gs', 'mp', 'tm', 'season', 'player_id', 'player', 'pos_PG', 'pos_SG', 'pos_PF', 'pos_SF', 'pos_C']]
-        df = pd.merge(df_subset.reset_index(), pd.DataFrame(pc_df).reset_index())
-        df = df.drop(columns=['index'])
-        df = df.rename(columns={x: str(x) for x in range(0, 30)})
-        column_titles = [str(x) for x in range(0, 30)]
-    else:
-        stats_df = df.drop(columns=non_stats_columns)
-        column_titles = stats_df.columns.tolist()
-        column_titles = [x for x in column_titles if x not in ['pos_PG', 'pos_SG', 'pos_PF', 'pos_SF', 'pos_C']]
+def create_potential_rosters(player_projections_df: pd.DataFrame, last_season_df: pd.DataFrame, team):
+    df = pd.DataFrame()
+    team_stats = last_season_df[last_season_df['tm'] == team]
+    player_stats = player_projections_df.drop(columns=['g', 'gs', 'mp', 'height', 'weight'])
 
-    # Reverse one-hot-encoding
-    for pos in 'PG', 'SG', 'PF', 'SF', 'C':
-        df[f'pos_{pos}1'] = df[f'pos_{pos}'].apply(lambda x: pos if x else '')
-    df['pos'] = df['pos_PG1'] + df['pos_SG1'] + df['pos_PF1'] + df['pos_SF1'] + df['pos_C1']
-    df = df.drop(columns=['pos_PG1', 'pos_SG1', 'pos_PF1', 'pos_SF1', 'pos_C1'])
-    df = df.drop(columns=['pos_PG', 'pos_SG', 'pos_PF', 'pos_SF', 'pos_C'])
+    for index, row in player_stats.iterrows():
 
-    # Aggregating player data to weighted average by position
-    total_min = df.groupby(by=['tm', 'season', 'pos']).sum()
-    total_min = total_min.reset_index()[['tm', 'season', 'pos', 'mp']]
-    total_min = total_min.rename(columns={'mp': 'total_mp'})
-    df = pd.merge(df, total_min, on=['tm', 'season', 'pos'])
-    df['mp%'] = df['mp'] / df['total_mp']
-    for x in column_titles:
-        df[x] = df[x] * df['mp%']
-    df = df.groupby(by=['tm', 'season', 'pos']).sum()
-    df = df.reset_index().drop(columns=['g', 'gs', 'mp', 'total_mp', 'mp%', 'player_id', 'player'])
+        if row['pos_PG']:
+            new_row = team_stats.iloc[0].copy()
+            new_row['player'] = row['player']
+            new_row[stat_columns_PG] = row[stat_columns]
+            df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
 
-    c_df = df[df['pos'] == 'C']
-    pg_df = df[df['pos'] == 'PG']
-    sg_df = df[df['pos'] == 'SG']
-    pf_df = df[df['pos'] == 'PF']
-    sf_df = df[df['pos'] == 'SF']
-    df = pd.merge(c_df, pg_df, on=['tm', 'season'], suffixes=('C', 'PG'))
-    df = pd.merge(df, sg_df, on=['tm', 'season'])
-    df = pd.merge(df, pf_df, on=['tm', 'season'], suffixes=('SG', 'PF'))
-    df = pd.merge(df, sf_df, on=['tm', 'season'])
-    df = df.rename(columns={x: f'{x}SF' for x in column_titles})
-    df = df.drop(columns=['pos', 'posC', 'posPG', 'posSG', 'posPF'])
+        if row['pos_SG']:
+            new_row = team_stats.iloc[0].copy()
+            new_row['player'] = row['player']
+            new_row[stat_columns_SG] = row[stat_columns]
+            df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
 
-    # Add win%
-    team_df = team_df[['Season', 'Team', 'W%']]
-    team_df = team_df.rename(columns={'Season': 'season', 'Team': 'tm'})
-    df = pd.merge(df, team_df, on=['season', 'tm'])
-    df = df.drop(columns=['tm', 'season'])
+        if row['pos_PF']:
+            new_row = team_stats.iloc[0].copy()
+            new_row['player'] = row['player']
+            new_row[stat_columns_PF] = row[stat_columns]
+            df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
 
-    if save_file:
-        assert isinstance(output_dir, str), 'Must provide directory in which to save output .csv file'
-        df.to_csv(f'{output_dir}/roster_win%.csv')
+        if row['pos_SF']:
+            new_row = team_stats.iloc[0].copy()
+            new_row['player'] = row['player']
+            new_row[stat_columns_SF] = row[stat_columns]
+            df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
 
-    return df"""
+        if row['pos_C']:
+            new_row = team_stats.iloc[0].copy()
+            new_row['player'] = row['player']
+            new_row[stat_columns_C] = row[stat_columns]
+            df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
+
+    return df
+
+
+def predict_win_percentages(potential_rosters_df: pd.DataFrame, model_path):
+    df = potential_rosters_df.drop(columns=['full_season', 'tm', 'season', 'player'])
+    model = joblib.load(model_path)
+    win_pred = model.predict(df)
+    df['win%'] = win_pred
+
+    return df[['player', 'win%']]
+
+
+def calculate_top_players_ui(selected_team: str, available_players: list) -> dict:
+
+    # Team Mappings
+    with open('../../data/processed/team_abbr_mapping.json', 'r') as file:
+        mappings = json.load(file)
+    team = mappings[selected_team]
+
+    # Loading dataframes
+    projections_df = pd.read_csv('../data/processed/rookie_projection_stats.csv')
+    last_season_df = pd.read_csv('../data/interim/NBA_team_player_stats_WA_current_year.csv')
+
+    # Filtering for available players
+    projections_df = projections_df[projections_df['player'].isin(available_players)]
+
+    # Make predictions
+    potential_rosters_df = create_potential_rosters(projections_df, last_season_df, team)
+    predictions = predict_win_percentages(potential_rosters_df, '../../models/win_prediction.pkl')
+
+    # Convert to dictionary
+    predictions = predictions.groupby(by='player').max().sort_values('win%', ascending=False)
+    predictions = predictions.to_dict()
+
+    return predictions
