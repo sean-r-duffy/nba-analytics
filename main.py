3331f28e7ce73c6a9ab562f13c5d4ed479270d69
sean-r-duffy <duffy.sean.14@gmail.com>
2024-08-05 13:06:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
 import webbrowser
+import toml
+
+
 
 
 def run_streamlit_app():
     os.chdir('src/ui')
+    config = toml.load('.streamlit/config.toml')
+    port = config.get('port', 8501)
+    webbrowser.open(f'http://localhost:{port}')
     os.system('streamlit run dashboard.py')
 
 
+
 if __name__ == "__main__":
-    webbrowser.open('http://localhost:8501')
     run_streamlit_app()
 
