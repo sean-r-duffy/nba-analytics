88e360b2386b8978d12e9e0bda20ffdabb997267
sean-r-duffy <duffy.sean.14@gmail.com>
2024-08-05 12:46:24 -0400

    New Branch for added features

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import os
+import webbrowser
 
+
+def run_streamlit_app():
+    os.chdir('src/ui')
+    os.system('streamlit run dashboard.py')
+
+
+if __name__ == "__main__":
+    webbrowser.open('http://localhost:8501')
+    run_streamlit_app()
